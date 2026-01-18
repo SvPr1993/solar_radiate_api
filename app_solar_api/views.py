c41275039df6fa6113e82ea37ec037b2ea34f01a
SvPr1993 <mrtventana@gmail.com>
2026-01-18 13:58:33 +0300

    solar_radiate_api

--- app_solar_api/views.py
+++ app_solar_api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from app_solar_api.service import random_num, random_num_2
 
-
+# Метод возвращает json с рандомными числами
 class Number(APIView):
     def get(self, request):
         int_num = random_num()
